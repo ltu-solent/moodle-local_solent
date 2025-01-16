d015e14e05bd77cfffb43c01cb9686e43f8859fe
Mark Sharp <mark.sharp@solent.ac.uk>
2025-01-16 09:30:50 +0000

    Class selector for main content has changed

--- amd/src/enrolments.js
+++ amd/src/enrolments.js
@@ -6,7 +6,7 @@ import Notification from 'core/notification';
 export const deleteContent = () => {
     const main = document.querySelector('body.courserole-none div[role="main"]');
     if (main) {
-        main.querySelector('.onetopic-tab-body').innerHTML = '';
+        main.querySelector('.course-content').innerHTML = '';
         Notification.addNotification({
             message: M.util.get_string('noroleerror', 'local_solent'),
             closebutton: false,
